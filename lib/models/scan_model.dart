185dd2e2f602f29e1b3dfdef26f2b69957f2c514
mzamor <zamoramariano@hotmail.com>
2022-01-30 21:46:39 -0300

    modelo scan, config android y importacion path_provider y sqflite

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+  ScanModel({
+    this.id = 1,
+    this.type = "https",
+    required this.value,
+  }){
+    if(type.contains('http')){
+      type = 'http';
+    }else{
+      type = 'geo';
+    }
+  }
+
+  int id;
+  String type;
+  String value;
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+    id: json["id"],
+    type: json["type"],
+    value: json["value"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "type": type,
+    "value": value,
+  };
+}
