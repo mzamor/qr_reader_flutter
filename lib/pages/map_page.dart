efceae4c054c9a27a11a6f8d7ec0b927e37c4e50
mzamor <zamoramariano@hotmail.com>
2022-01-31 21:34:33 -0300

    change MapsPage

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -5,9 +5,14 @@ class MapsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Center(
-          child: Text('Maps Page')),
-    );
+    return  ListView.builder(
+      itemCount: 10,
+        itemBuilder: (_, index) => ListTile(
+          leading: Icon(Icons.map, color: Theme.of(context).primaryColor,),
+          title: const Text('http://www.terra.com.ar'),
+          subtitle: const Text('ID: 1'),
+          trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
+          onTap: () => debugPrint('open something...'),
+        ),);
   }
 }
